6996231038f00054617cd2f63d5c1f85a0e803de
PerpetuumG <perpetuumg@gmail.com>
2024-03-25 16:39:19 +0200

    create Navbar

---
+++ src/app/(user)/page.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import Banner from '@/components/Banner';
+
+const HomePage: FC = () => {
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/components/Banner.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+
+const Banner: FC = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, tempore!</p>
+        </div>
+    );
+};
+
+export default Banner;
