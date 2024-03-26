de75ab8242e35c420817081ea956de2726770ce0
PerpetuumG <perpetuumg@gmail.com>
2024-03-26 19:09:13 +0200

    add Slider

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,11 +1,32 @@
 import React, { FC } from 'react';
 import Banner from '@/components/Banner';
+import { groq } from 'next-sanity';
+import { client } from '@/lib/sanityClient';
 
-const HomePage: FC = () => {
+export const revalidate = 10;
+const bannerQuery = groq`*[_type == 'banner']{
+  image,
+  _id
+} | order(_createdAt asc)`;
+
+const newArrivalQuery = groq`*[_type == 'product' && position == 'New Arrivals']{
+...
+} | order(_createdAt asc)`;
+
+const bestSellersQuery = groq`*[_type == 'product' && position == 'Bestsellers']{
+  ...
+ } | order(_createdAt asc)`;
+const specialOffersQuery = groq`*[_type == 'product' && position == 'Special Offers']{
+  ...
+ } | order(_createdAt asc)`;
+
+const HomePage: FC = async () => {
+  const banners = await client.fetch(bannerQuery);
+
   return (
-    <div>
-      <Banner />
-    </div>
+    <main className={'text-sm overflow-hidden min-h-screen'}>
+      <Banner banners={banners} />
+    </main>
   );
 };
 
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,11 +1,134 @@
-import React, {FC} from 'react';
+'use client';
+import Slider from 'react-slick';
 
-const Banner: FC = () => {
-    return (
-        <div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, tempore!</p>
-        </div>
-    );
+import React, { FC, useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { urlFor } from '@/lib/sanityClient';
+
+const Banner: FC = ({ banners }: any) => {
+  const [dotActive, setDotActive] = useState(0);
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    beforeChange: (prev: any, next: any) => {
+      setDotActive(next);
+    },
+    appendDots: (dots: any) => (
+      <div
+        style={{
+          position: 'absolute',
+          top: '50%',
+          left: '7%',
+          transform: 'translateY(-50%)',
+        }}
+      >
+        <ul style={{ margin: '0px' }}> {dots} </ul>
+      </div>
+    ),
+    customPaging: (i: any) => (
+      <div
+        style={
+          i === dotActive
+            ? {
+                width: '30px',
+                color: '#262626',
+                borderRight: '3px #262626 solid',
+                padding: '8px 0',
+                cursor: 'pointer',
+              }
+            : {
+                width: '30px',
+                color: 'transparent',
+                borderRight: '3px white solid',
+                padding: '8px 0',
+                cursor: 'pointer',
+              }
+        }
+      >
+        <p
+          style={
+            i === dotActive
+              ? { color: 'black', fontWeight: '600' }
+              : { color: 'white', fontWeight: '600' }
+          }
+        >
+          {' '}
+          0{i + 1}
+        </p>
+      </div>
+    ),
+    responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          dots: true,
+          appendDots: (dots: any) => (
+            <div
+              style={{
+                position: 'absolute',
+                top: '50%',
+                left: '2%',
+                transform: 'translateY(-50%)',
+              }}
+            >
+              <ul style={{ margin: '0px' }}> {dots} </ul>
+            </div>
+          ),
+          customPaging: (i: any) => (
+            <div
+              style={
+                i === dotActive
+                  ? {
+                      width: '25px',
+                      color: '#262626',
+                      borderRight: '3px #262626 solid',
+                      cursor: 'pointer',
+                      fontSize: '12px',
+                    }
+                  : {
+                      width: '25px',
+                      color: 'transparent',
+                      borderRight: '3px white solid',
+                      cursor: 'pointer',
+                      fontSize: '12px',
+                    }
+              }
+            >
+              0{i + 1}
+            </div>
+          ),
+        },
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Slider {...settings}>
+        {banners?.map((item: any) => (
+          <Link href={'/shop'} key={item.key} className={'relative'}>
+            <Image
+              src={urlFor(item.image).url()}
+              alt={'banner image'}
+              width={2000}
+              height={2000}
+              className={'w-full max-h-[650px] object-cover '}
+            />
+            <div
+              className={
+                'w-full h-40 bg-gradient-to-t from-gray-100 to-transparent absolute bottom-0 z-20'
+              }
+            />
+          </Link>
+        ))}
+      </Slider>
+    </div>
+  );
 };
 
 export default Banner;
