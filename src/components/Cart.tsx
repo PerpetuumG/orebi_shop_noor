9fa546adf9a021e7813e34d15dd7563396ee73ff
PerpetuumG <perpetuumg@gmail.com>
2024-04-02 15:48:52 +0300

    add Stripe, checkout, success

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -12,11 +12,14 @@ import emptyCart from '@/assets/emptyCart.png';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
 import Price from '@/components/Price';
+import { loadStripe } from '@stripe/stripe-js';
+import { useSession } from 'next-auth/react';
 
 const Cart = () => {
   const { productData } = useSelector((state: StateProps) => state.orebi);
   const dispatch = useDispatch();
   const [totalAmt, setTotalAmt] = useState(0);
+  const { data: session } = useSession();
 
   useEffect(() => {
     let price = 0;
@@ -33,6 +36,28 @@ const Cart = () => {
 
     confirmed && dispatch(resetCart());
     toast.success('Cart resetted successfully!');
+  };
+
+  // Stripe payment
+  const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+  const createCheckout = async () => {
+    if (session?.user) {
+      const stripe = await stripePromise;
+      const response = await fetch('http://localhost:3000/api/checkout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'appication/json' },
+        body: JSON.stringify({
+          items: productData,
+          email: session?.user?.email,
+        }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        stripe?.redirectToCheckout({ sessionId: data.id });
+      }
+    } else {
+      toast.error('Please sign in to make Checkout');
+    }
   };
 
   return (
@@ -113,6 +138,7 @@ const Cart = () => {
 
               <div className={'flex justify-end'}>
                 <button
+                  onClick={createCheckout}
                   className={'w-52 h-10 bg-primeColor text-white hover:bg-black duration-300'}
                 >
                   Proceed to Checkout
